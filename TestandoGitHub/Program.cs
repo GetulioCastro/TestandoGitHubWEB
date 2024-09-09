2b4b3873826e008eb5723daf0542c1a0f28c3368
Getulio Augusto Ferreira de Castro Junior <augustoge71@gmail.com>
2024-09-09 10:16:07 -0300

    Add project files.

---
+++ TestandoGitHub/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestandoGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual seu primeiro nome? ");
+            Console.ReadLine();
+
+        }
+    }
+}
